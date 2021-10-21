d3caf6912430567a96133fc265b78c3a7e1ccdb5
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-21 21:09:04 +0000

    update

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ Merge two sorted linked lists and return it as a sorted list. The list should be
 
 Ex1) Input: l1 = [1,2,4], l2 = [1,3,4]
 Output: [1,1,2,3,4,4]
+
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  
+}
